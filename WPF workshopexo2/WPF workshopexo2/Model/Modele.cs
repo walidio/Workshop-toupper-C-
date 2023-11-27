2ad624dbf6ae45c992b3beb9241f4a9a7127ab06
walidio <74817560+walidio@users.noreply.github.com>
2023-11-27 11:59:26 +0100

    Add files via upload

---
+++ WPF workshopexo2/WPF workshopexo2/Model/Modele.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_workshopexo2.Model
+{
+
+        class Modele
+        {
+            public string toupper(string Input)
+            {
+                string output = Input.ToUpper();
+                return output;
+            }
+        }
+}
---
+++ WPF workshopexo2/WPF workshopexo2/VuModel/VuModele.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using WPF_workshopexo2.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace WPF_workshopexo2.VuModel
+{
+    internal class VuModele : ViewModelBase
+    {
+        private string _userInput;
+
+
+        public string UserInput
+        {
+            get => UserInput;
+            set
+            {
+                Set(ref _userInput, value);
+                RaisePropertyChanged(nameof(UserInput));
+            }
+        }
+        private string _result;
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                Set(ref _result, value);
+                RaisePropertyChanged(nameof(Result));
+            }
+        }
+        public ICommand ConvertCommand { get; }
+        public VuModele()
+        {
+            ConvertCommand = new RelayCommand(convert);
+        }
+        public void convert()
+        {
+            Modele model=new Modele();
+            Result = model.toupper(UserInput);
+        }
+
+    }
+}
+
